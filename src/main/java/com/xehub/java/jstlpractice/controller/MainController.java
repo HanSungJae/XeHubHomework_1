35b0a4d5325ebe5d6ac1b2180af2b05dd9b4b4c2
HanSungJae <citizen930@naver.com>
2022-08-29 12:03:28 +0900

    수정사항 중간 저장

--- src/main/java/com/xehub/java/jstlpractice/controller/MainController.java
+++ src/main/java/com/xehub/java/jstlpractice/controller/MainController.java
@@ -4,6 +4,7 @@ import com.xehub.java.jstlpractice.service.ChatService;
 import com.xehub.java.jstlpractice.service.FAQService;
 import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,14 +19,23 @@ public class MainController {
     @Resource
     ChatService chatService;
 
+//    @GetMapping("")
+//    public String main(Model model) {
+//
+//        model.addAttribute("isLoggedIn", Math.floor(Math.random() * 2) > 0);
+//        model.addAttribute("faqList", faqService.getFAQList());
+//        model.addAttribute("chatService", chatService.getChatList());
+//
+//        return "main";
+//    }
     @GetMapping("")
     public ModelAndView main(ModelAndView modelAndView) {
-        modelAndView.setViewName("main");
 
         modelAndView.addObject("isLoggedIn", Math.floor(Math.random() * 2) > 0);
         modelAndView.addObject("faqList", faqService.getFAQList());
         modelAndView.addObject("chatService", chatService.getChatList());
 
+        modelAndView.setViewName("main");
         return modelAndView;
     }
 }
